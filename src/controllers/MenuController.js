05cb8e6150c5ff66ae86edff33ef30b1088497b1
Long Phi <longphics@gmail.com>
2022-05-25 23:04:06 +0700

    add feature

--- src/controllers/MenuController.js
+++ src/controllers/MenuController.js
@@ -1,13 +1,13 @@
 const Customer = require('../models/Customer.js')
 const Menu = require('../models/Menu.js')
-const func = require('./func.js')
+const createMenu = require('./Func.js').createMenu
 
 class MenuController {
     // [GET] /menu
     index(req, res) {
         Menu.find({}, (err, docs) => {
             if (docs.length == 0) {
-                func.createMenu()
+                createMenu()
             } else {
                 docs = docs.map(doc => doc.toObject())
                 res.render('menu', { docs })
