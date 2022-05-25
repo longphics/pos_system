91654e8bbf3721b4d901ea28e41708df1119cb18
Long Phi <longphics@gmail.com>
2022-05-26 00:29:48 +0700

    add feature

--- src/database.js
+++ src/database.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/pos_system')
+        await mongoose.connect('mongodb://0.0.0.0:27017/pos_system')
         console.log('Connected to database')
     } catch (error) {
-        console.log('Error when connect to database!')
+        console.log('Error when connect to database!\n' + error)
     }
 }
 
